8509345b08003bfec290ebc038b472f34cb7f298
Ehab Elia <ehab.elia@gmail.com>
2016-04-05 16:31:44 +0200

    erase devise

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ gem 'rails', '4.2.5'
 gem 'pg'
 gem 'figaro'
 gem 'jbuilder', '~> 2.0'
-gem 'devise'
 
 gem 'sass-rails', '~> 5.0'
 gem 'jquery-rails'
--- db/migrate/20160404142113_devise_create_users.rb
+++ db/migrate/20160404142113_devise_create_users.rb
@@ -1,5 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
+
     create_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
@@ -38,5 +39,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
+  
+    drop_table :users
   end
 end
